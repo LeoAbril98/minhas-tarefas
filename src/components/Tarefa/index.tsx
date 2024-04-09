5fb6d764d30ada0f4adff3158b48a4b6cdd3ab9b
Leonardo Abril <leoabril98@gmail.com>
2024-04-09 20:44:45 -0300

    commit final

---
+++ src/components/Tarefa/index.tsx
@@ -0,0 +1,103 @@
+import { ChangeEvent, useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import * as S from './styles'
+import { remover, editar, alteraStatus } from '../../store/reducer/tarefas'
+import TarefaClass from '../../models/Tarefa'
+import { Botao, BotaoSalvar } from '../../styles'
+import * as enums from '../../utils/enums/Tarefa'
+
+type Props = TarefaClass
+
+const Tarefa = ({
+  descricao: descricaoOriginal,
+  prioridade,
+  titulo,
+  status,
+  id
+}: Props) => {
+  const dispath = useDispatch()
+  const [estaEditando, setEstaEditando] = useState(false)
+  const [descricao, setDescicao] = useState('')
+
+  useEffect(() => {
+    if (descricaoOriginal.length > 0) {
+      setDescicao(descricaoOriginal)
+    }
+  }, [descricaoOriginal])
+
+  function cancelarEdicao() {
+    setEstaEditando(false)
+    setDescicao(descricaoOriginal)
+  }
+
+  function alteraStatusTarefa(evento: ChangeEvent<HTMLInputElement>) {
+    dispath(
+      alteraStatus({
+        id,
+        finalizado: evento.target.checked
+      })
+    )
+  }
+  return (
+    <S.Card>
+      <label htmlFor={titulo}>
+        <input
+          type="checkbox"
+          id={titulo}
+          checked={status === enums.Status.CONCLUIDA}
+          onChange={alteraStatusTarefa}
+        />
+        <S.Titulo>
+          {estaEditando && <em>Editando: </em>}
+          {titulo}
+        </S.Titulo>
+      </label>
+      <S.Tag parametro="prioridade" prioridade={prioridade}>
+        {prioridade}
+      </S.Tag>
+      <S.Tag parametro="status" status={status}>
+        {status}
+      </S.Tag>
+      <S.Descricao
+        disabled={!estaEditando}
+        value={descricao}
+        onChange={(evento) => setDescicao(evento.target.value)}
+      />
+      <S.BarraAcoes>
+        {estaEditando ? (
+          <>
+            <BotaoSalvar
+              onClick={() => {
+                dispath(
+                  editar({
+                    descricao,
+                    prioridade,
+                    titulo,
+                    status,
+                    id
+                  })
+                )
+                setEstaEditando(false)
+              }}
+            >
+              Salvar
+            </BotaoSalvar>
+            <S.BotaoCancelarRemover onClick={cancelarEdicao}>
+              Cancelar
+            </S.BotaoCancelarRemover>
+          </>
+        ) : (
+          <>
+            <Botao onClick={() => setEstaEditando(true)}>Editar</Botao>
+            <S.BotaoCancelarRemover onClick={() => dispath(remover(id))}>
+              Remover
+            </S.BotaoCancelarRemover>
+          </>
+        )}
+      </S.BarraAcoes>
+    </S.Card>
+  )
+}
+
+export default Tarefa
